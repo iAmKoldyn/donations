170a8c9b4e16fe437821df6b91465d842d549b95
iAmKoldyn <pataraya951@gmail.com>
2023-09-22 23:59:46 +0300

    endpoint_success

--- src/routers/subscriberRouter.js
+++ src/routers/subscriberRouter.js
@@ -1,11 +1,20 @@
-const { subscriberResponse } = require('../stubs');
+const { subscriberSuccessResponse, subscriberFailureResponse} = require('../stubs');
 
 async function routes(fastify, options) {
     fastify.get('/subscribers', async (request, reply) => {
-        return reply
-            .code(200)
-            .header('Content-Type', 'application/json; charset=utf-8')
-            .send(subscriberResponse);
+        const shouldFail = request.query.fail === 'true';
+
+        if (shouldFail) {
+            return reply
+                .code(400)
+                .header('Content-Type', 'application/json; charset=utf-8')
+                .send(subscriberFailureResponse);
+        } else {
+            return reply
+                .code(200)
+                .header('Content-Type', 'application/json; charset=utf-8')
+                .send(subscriberSuccessResponse);
+        }
     });
 }
 
