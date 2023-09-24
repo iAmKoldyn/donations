c0e70ba994885e9c12b53e1ed5e53fae7aaf28b0
Yakov Yatchenko <yakovyatchenko@yandex.ru>
2023-09-24 12:01:18 +0000

    add endpoints

--- src/routers/authorRouter.js
+++ src/routers/authorRouter.js
@@ -1,20 +1,55 @@
-const { authorSuccessResponse, authorFailureResponse } = require('../stubs');
-
 async function routes(fastify, options) {
-    fastify.get('/authors', async (request, reply) => {
-        const shouldFail = request.query.fail === 'true';
+    fastify.get('/authors/:id', async (request, reply) => {
+        //* TODO - retrieve author by id, return 200 or 404
 
-        if (shouldFail) {
-            return reply
-                .code(400)
-                .header('Content-Type', 'application/json; charset=utf-8')
-                .send(authorFailureResponse);
-        } else {
+        const { id } = request.params;
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send(`*serialized author with id: ${id}*`);
+    });
+
+    fastify.get('/authors', async (request, reply) => {
+        //* TODO - retrieve authors collection, don't wrap in try-catch or wrap with not 500 code in catch
+        //* TODO - if param tags is present, return authors collection by tags
+
             return reply
                 .code(200)
                 .header('Content-Type', 'application/json; charset=utf-8')
-                .send(authorSuccessResponse);
-        }
+                .send("*authors collection*");
+    });
+
+    fastify.post('/authors', async (request, reply) => {
+        //* TODO - check request parameters, create author, return 200, 422 or 409
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send("author with id: *id* created!")
+    });
+
+    fastify.delete('/authors/:id', async (request, reply) => {
+        //* TODO - find and delete author by id, return 200 or 404
+
+        const { id } = request.params;
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send(`author with id: ${id} deleted!`)
+    });
+
+    fastify.put('/authors/:id', async (request, reply) => {
+        //* TODO - find and update author by id, return 200, 404 or 422 if params are incorrect
+
+        const { id } = request.params;
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send({message: `author with id: ${id} updated!`,
+                    "author": "*serialized author*"})
     });
 }
 
---
+++ src/routers/subscriptionRouter.js
@@ -0,0 +1,35 @@
+async function routes(fastify, options) {
+    fastify.get('/subscriptions/:id', async (request, reply) => {
+        //* TODO - retrieve subscription by id, return 200 or 404
+
+        const { id } = request.params;
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send(`*serialized subscription with id: ${id}*`);
+    });
+
+    fastify.get('/subscriptions', async (request, reply) => {
+        //* TODO - retrieve subscriptions collection, don't wrap in try-catch or wrap with not 500 code in catch
+        //* TODO - if authorId param is present, get subscribers collection by authorId
+        //* TODO - if subscriberId param is present, get subscribers collection by subscriberId
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send("*subscriptions collection*");
+    });
+
+    fastify.post('/subscriptions', async (request, reply) => {
+        //* TODO - check request parameters, create subscription, return 200, 422
+        //* TODO - 409 if there is subscription with same level, userId and authorId and it's actual
+
+        return reply
+            .code(200)
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send("subscription with id: *id* created!")
+    });
+}
+
+module.exports = routes;
